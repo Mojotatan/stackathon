d03f191b8e46597999ee792cf479dc83d8e6041b
Mojotatan <tatan42@gmail.com>
2017-04-28 15:14:20 -0500

    moving from local to sockets

--- browser/app.js
+++ browser/app.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux'
 import {BrowserRouter, Route} from 'react-router-dom'
 import store from './redux/store'
 
-const socket = io(window.location.origin)
+// const socket = io(window.location.origin)
 
 import Canvas from './components/canvas'
 
